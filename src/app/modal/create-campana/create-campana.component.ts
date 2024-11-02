69251acd009c80a717fad7aa3840aa40e12d7d0b
mariajosecq <cote21cq@gmail.com>
2024-11-01 22:03:39 -0300

    En modal de creacion de campaña se restringe horario para seleccionar entre 8:00 y 22:00 por requerimiento del caso

--- src/app/modal/create-campana/create-campana.component.ts
+++ src/app/modal/create-campana/create-campana.component.ts
@@ -16,15 +16,29 @@ export class CreateCampanaComponent {
   @Output() onCreate = new EventEmitter<campana>();
   @Output() onClose = new EventEmitter<void>();
 
-  currentDate: string = '';
+  currentDate: string;
+  availableHours: string[] = [];
 
   constructor() {
     const today = new Date();
-    this.currentDate = today.toISOString().split('T')[0]; // Obtiene la fecha en formato YYYY-MM-DD
+    this.currentDate = today.toISOString().split('T')[0]; // Obtener la fecha en formato YYYY-MM-DD
+    this.generateAvailableHours();//Obtener horas en el rango disponible
   }
 
   save() {    
-    this.onCreate.emit(this.campana); // Emite los datos de la nueva campaña
-  }  
+    this.onCreate.emit(this.campana); // Emitir los datos de la nueva campaña
+  }
+  
+  generateAvailableHours() {
+    const startHour = 8; // 08:00
+    const endHour = 22; // 22:00
+
+    for (let hour = startHour; hour <= endHour; hour++) {
+      for (let minutes of [0, 15, 30, 45]) {
+        if (hour === endHour && minutes > 0) break; // Evitar horas después de 22:00
+        this.availableHours.push(`${hour.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`);
+      }
+    }
+  }
 
 }
